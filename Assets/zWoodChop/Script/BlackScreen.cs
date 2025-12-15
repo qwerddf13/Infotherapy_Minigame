ead8684cbde54919f4033ef7d719c31b93206b7f
qwerddf13 <i58598085@gmail.com>
2025-12-15 23:35:52 +0900

    사소해

--- Assets/zWoodChop/Script/BlackScreen.cs
+++ Assets/zWoodChop/Script/BlackScreen.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlackScreen : MonoBehaviour
+public class BlackScreenWoodChop : MonoBehaviour
 {
     GameObject blackScreen;
     void Start()
